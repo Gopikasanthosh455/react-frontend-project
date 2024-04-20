c70c553ce692c02f73fb8fe80ec038fbec9f3577
Gopika santhosh <165985827+Gopikasanthosh455@users.noreply.github.com>
2024-04-20 20:09:48 +0530

    Add files via upload

---
+++ src/dashboard/AddDetailsDashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../Navbar/Navbar'
+import AddDetails from '../component/AddDetails/AddDetails'
+
+ 
+function AddDetailsDashboard() {
+  return (
+    <div>
+      <NavBar/>
+      <AddDetails/>
+    </div>
+  )
+}
+
+export default AddDetailsDashboard
